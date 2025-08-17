f8be9e8d248d821b0f7ec6905fc5204d157f3f40
Villalobos87 <jmvillalobos87@gmail.com>
2025-08-17 10:02:58 -0600

    2V

--- app.py
+++ app.py
@@ -163,7 +163,7 @@ AgGrid(
 
 total_cuota_cancelada = df[df["Estado"]=="Cancelado"]["Cuota"].sum()
 Capital_Inicial = 9000
-Ganancias_Entregadas =3698.24
+Ganancias_Entregadas =3698.24-698.24
 Efectivo = total_cuota_cancelada + Capital_Inicial - total_prestado - Ganancias_Entregadas
 Pendiente_Recuperar = df[df["Estado"]=="Pendiente"]["Cuota"].sum()
 
